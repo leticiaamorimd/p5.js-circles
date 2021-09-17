928b04b27243d1fc7617c40717e809c15052e2c1
Leticia Amorim <66920249+leticiaamorimd@users.noreply.github.com>
2021-09-17 23:28:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,15 +12,10 @@ function setup() {
 function draw() {
   background(0, 15);
 
-  // TODO: Call the makeCircle() function with 4 for xPos, 50 for yPos and 600 for circleSize
-makeCircle(4, 50, 600)
-
-  // TODO: Call the makeCircle() function with 0 for xPos, 200 for yPos and 200 for circleSize
-makeCircle(0, 200, 200)
-
-  // TODO: Call the makeCircle() function with 100 for xPos, 200 for yPos and 1 for circleSize
+  makeCircle(4, 50, 600)
+  makeCircle(0, 200, 200)
   makeCircle(100, 200, 1)
-  makeCircle(200, 200, 9)
+  makeCircle(200, 200, 2)
   makeCircle(100, 200, 2)
   makeCircle(100, 200, 5)
 
